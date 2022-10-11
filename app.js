63e3937ada634ded14a9508a74d25fd68f3d6b23
yusuf-eren <erenyusuf170@gmail.com>
2022-10-11 17:05:54 +0300

    x

--- app.js
+++ app.js
@@ -25,10 +25,35 @@ app.get("/buy-emir", async (req, res) => {
       pricing_type: "fixed_price",
     }),
   };
+});
+
+
+app.get("/buy-efe", async (req, res) => {
+  const reqUrl = "https://api.commerce.coinbase.com/charges/";
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "X-CC-Api-Key": "583ddb17-3714-4a7e-86e6-10ae7e7e7152",
+      "X-CC-Version": "2018-03-22",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      local_price: { amount: 0.041, currency: "ETH" },
+      redirect_url: "http://localhost:3000/accepted",
+      cancel_url: "http://localhost:3000/cancelled",
+      pricing_type: "fixed_price",
+    }),
+  };
+
+  const response = await fetch(reqUrl, options);
+  const data = await response.json();
+  res.render("efe", {
 
   const response = await fetch(reqUrl, options);
   const data = await response.json();
   res.render("emir", {
+
     eth_address: data["data"]["addresses"]["ethereum"],
     payment_link: data["data"]["hosted_url"],
   });
